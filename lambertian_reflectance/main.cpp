f695d95a0c16e269f2dcdf377d1c0a5d1802a571
avoup <avoup.g@gmail.com>
2020-06-10 21:46:29 +0400

    lambertian reflectance fix

--- lambertian_reflectance/main.cpp
+++ lambertian_reflectance/main.cpp
@@ -7,14 +7,6 @@ struct Point
 {
 	int x, y, z;
 };
-
-int side(Point P0, Point P1, Point C)
- {
-   double M = (C.y-P0.y)*(P1.x-P0.x) - (C.x-P0.x)*(P1.y-P0.y);
-   	
-   if (M > 0) return 1;
-   return 0;
- }
    
 main ()  
  {
@@ -30,21 +22,7 @@ main ()
 
    initwindow(width, height);   
    cleardevice();   
-   
-   // trianlge coords
-    Point A[3];
-	A[0].x = 50; A[0].y = 50; A[0].z = 0;   
-	A[1].x = 450; A[1].y = 50; A[1].z = 0;   
-	A[2].x = 250; A[2].y = 450; A[2].z = 0;   
-	
-    // draw triangle lines
-    for (int i = 0; i < 3; i++)
-		line(A[i].x, A[i].y, A[(i+1)%3].x, A[(i+1)%3].y );
-   
-//	int minX = min(min(A[0].x, A[1].x), A[2].x);
-//	int minY = min(min(A[0].y, A[1].y), A[2].y);
-//	int maxX = max(max(A[0].x, A[1].x), A[2].x);
-//	int maxY = max(max(A[0].y, A[1].y), A[2].y);
+
 	int minX = 0;
 	int minY = 0;
 	int maxX = width;
@@ -71,10 +49,7 @@ main ()
 			r = (255 * intensity) + r > 255 ? 255 : (int) 255 * intensity + r;
 			g = (255 * intensity) + g > 255 ? 255 : (int) 255 * intensity + g;
 			b = (255 * intensity) + b > 255 ? 255 : (int) 255 * intensity + b;
-			
-			int sum = side(A[0], A[1], curr) + side(A[1], A[2], curr) + side(A[2], A[0], curr);
-						
-//			if (sum == 3 || sum == 0) putpixel(x, y, COLOR(r, g, b));
+
 			putpixel(x, y, COLOR(r, g, b));
 		}
 	}
