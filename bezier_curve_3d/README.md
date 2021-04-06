957bbcb7c87bb68053dbb6b8710626b3b17c5cf4
Lasha Gog <avoup.g@gmail.com>
2021-04-06 13:29:39 +0400

    add readme; add comments

---
+++ bezier_curve_3d/README.md
@@ -0,0 +1,24 @@
+# Bezier curve 3d
+
+Bezier curve implementation in c++ using graphics.h library.
+
+## Usage
+
+All the code resides in main.cpp file.
+Run bezier_curve_3d.exe file to draw bezier.
+
+## Overview
+
+Screenshots of figures.
+
+### Simple bezier curve with projections
+![bezier curve 3d](/screenshots/bezier_simple.jpg)
+
+### Bezier curve with dropped lines on planes
+![bezier dropped lines](/screenshots/bezier_line_drop.jpg)
+
+### Simple wireframe cube
+![cube](/screenshots/cube.jpg)
+
+### Bezier inside wireframe cube
+![bezier inside wireframe cube](/screenshots/bezier_in_cube.jpg)
--- bezier_curve_3d/main.cpp
+++ bezier_curve_3d/main.cpp
@@ -4,6 +4,7 @@
 #include<iostream>
 using namespace std;
 
+// Define structures
 struct coord2D 
 {
 	double x, y;
@@ -18,7 +19,7 @@ struct coordSystem
 	double alpha;
 };
 
-// calculate 3d point coordinate on 2d plane
+// Calculate 3d point projection coordinate on 2d plane
 coord2D calculate2DPoint(coordSystem base, coord3D p)
 {
 	return coord2D {
@@ -27,7 +28,7 @@ coord2D calculate2DPoint(coordSystem base, coord3D p)
 	};
 }
 
-// draw 3d coordinate on 2d plane
+// Draw 3d coordinate on 2d plane
 void drawPixel3D(coordSystem base, coord3D p, int color)
 {
 	coord2D d = calculate2DPoint(base, p);
@@ -49,6 +50,7 @@ void drawLine3D(coordSystem base, coord3D p0, coord3D p1, int color, short style
 	line (x0, y0, x1, y1);
 };
 
+// Draw axises X, Y, Z
 void drawCoordSystem (coordSystem base, int size) 
 {		
 	coord3D p0  = {0,    0,    0};
@@ -63,7 +65,7 @@ void drawCoordSystem (coordSystem base, int size)
 
 void drawCube(coordSystem base, coord3D start, int size = 150, int style = 0, int color = WHITE, int border = 1)
 {	
-	// vertices
+	// Vertices
 	coord3D p0  = {start.x,        start.y,        start.z};
 	coord3D p1x = {start.x + size, start.y,        start.z};
 	coord3D p1y = {start.x,        start.y + size, start.z};
@@ -74,7 +76,7 @@ void drawCube(coordSystem base, coord3D start, int size = 150, int style = 0, in
 	coord3D p1yz  = {start.x,        start.y + size, start.z - size};	
 	coord3D p1xz  = {start.x + size, start.y,        start.z - size};
 	
-	// draw lines between vertices
+	// Draw lines between vertices
 	drawLine3D ( base, p0,    p1x,  color, style, border );
 	drawLine3D ( base, p0,    p1y,  color, style, border );
 	drawLine3D ( base, p0,    p1z,  color, style, border );
@@ -94,9 +96,10 @@ void drawCube(coordSystem base, coord3D start, int size = 150, int style = 0, in
 	drawLine3D ( base, p1z,   p1xz, color, style, border );
 }
 
-// draw 3d bezier curve with projections
-void bezier3D (coordSystem base, coord3D points[], double increment)
+// Draw 3d bezier curve with projections
+void bezier3D (coordSystem base, coord3D points[], double increment, int projEvery = 0)
 {
+	int iterNum = 0;
 	for(double t = 0; t <= 1; t += increment)
 	{
 		double x = points[0].x*pow((1-t), 3) + 3*points[1].x*pow((1-t), 2)*t + 3*points[2].x*(1-t)*t*t + points[3].x*t*t*t;
@@ -108,20 +111,19 @@ void bezier3D (coordSystem base, coord3D points[], double increment)
 		coord3D py = {x, 0, z};
 		coord3D pz = {x, y, 0};
 						
-		drawPixel3D(base, p,  COLOR(220, 220, 220));
-		drawPixel3D(base, px, COLOR(120, 120, 20)); // no X
-		drawPixel3D(base, py, COLOR(120, 20, 120)); // no Y
-		drawPixel3D(base, pz, COLOR(20, 120, 120));	// no Z
+		drawPixel3D(base, p,  COLOR(220, 220, 220)); // in 3d plane
+		drawPixel3D(base, px, COLOR(120, 120, 20)); // projection on YZ plane (x = 0)
+		drawPixel3D(base, py, COLOR(120, 20, 120)); // projection on XZ plane (y = 0)
+		drawPixel3D(base, pz, COLOR(20, 120, 120));	// projection on XY plane (z = 0)
 		
-//		cout << t << endl;
-
-//		if ( t == 0.03 )
-//		{
-//			cout << "meh" << endl;
-//			drawLine3D(base, p, px, COLOR(0, 200, 200));
-//			drawLine3D(base, p, py, COLOR(200, 0, 200));
-//			drawLine3D(base, p, pz, COLOR(200, 200, 0));
-//		}
+		// drop line on plane
+		iterNum++;
+		if (projEvery != 0 && iterNum % projEvery == 0 )
+		{
+			drawLine3D(base, p, px, COLOR(200, 200, 0));
+			drawLine3D(base, p, py, COLOR(200, 0, 200));
+			drawLine3D(base, p, pz, COLOR(0, 200, 200));
+		}
 	}
 }
 
@@ -131,7 +133,7 @@ int main()
 	
 	int w = 800;
 	int h = 800;
-	double a = 0.4;
+	double a = 0.4; // View angle
 	
 	initwindow(w, h);
 	cleardevice();	
@@ -145,7 +147,9 @@ int main()
 	points[2] = {80, 160, -180};
 	points[3] = {150, 50, -210};
 	
-	bezier3D(base, points, 0.001); 
+	
+	bezier3D(base, points, 0.001, 0); // Draw bezier
+	drawCube(base, {0, 0, 0}, 250); // Draw cube
 	
 	getch();	
 	closegraph();
